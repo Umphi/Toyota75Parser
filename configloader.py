bc3fcdecf91a3a734ed0d50b015322906889b658
Илья Юркевич <iforza1@yandex.ru>
2023-05-29 11:50:06 +0700

    added check if config file exists

--- configloader.py
+++ configloader.py
@@ -1,6 +1,9 @@
-import configparser
+import configparser, os
 
 def load_config(filename):
+    if not os.path.exists(f"./settings.ini"):
+        print("Config file (settings.ini) is not provided. Please, use \"settings.ini.example\" to create config.")
+        exit(1)
     config = configparser.ConfigParser()
     config.read(filename)
     return config
